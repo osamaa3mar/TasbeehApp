559f914ca63fdfafa21fdec6b62f0e8eb549e652
osamaa3mar <53447224+osamaa3mar@users.noreply.github.com>
2023-11-22 00:44:54 +0300

    Add files via upload

---
+++ Prayer/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Prayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumberOfPrayers = new System.Windows.Forms.MaskedTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Amiri Quran", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(214, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 132);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "توقف";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "تسبيح";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Amiri Quran", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(363, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 132);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ابدأ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(57, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 108);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Amiri Quran", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(398, 1);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(127, 87);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "التسبيح كل";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Amiri Quran", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(214, 1);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(69, 87);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ثانية";
+            // 
+            // txtNumberOfPrayers
+            // 
+            this.txtNumberOfPrayers.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberOfPrayers.Location = new System.Drawing.Point(313, 31);
+            this.txtNumberOfPrayers.Mask = "00000";
+            this.txtNumberOfPrayers.Name = "txtNumberOfPrayers";
+            this.txtNumberOfPrayers.Size = new System.Drawing.Size(68, 31);
+            this.txtNumberOfPrayers.TabIndex = 7;
+            this.txtNumberOfPrayers.Text = "120";
+            this.txtNumberOfPrayers.ValidatingType = typeof(int);
+            this.txtNumberOfPrayers.TextChanged += new System.EventHandler(this.txtNumberOfPrayers_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.IndianRed;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(577, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Hide";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 247);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtNumberOfPrayers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Prayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox txtNumberOfPrayers;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Prayer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prayer
+{
+    public partial class Form1 : Form
+    {
+        int Counter = 0;
+        int progress = 0;
+        Random rand= new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval= (Convert.ToInt32(txtNumberOfPrayers.Text) * 1000);
+           
+        }
+
+        string TextGenerator()
+        {
+            string[] prayers = new string[]
+            {
+        "الله أكبر",
+        "سبحان الله",
+        "الحمد لله",
+        "لا إله إلا الله",
+        "أستغفر الله",
+        "اللهم صل على سيدنا محمد",
+        "رب اغفر لي ولوالدي",
+        "اللهم إني أعوذ بك من الهم والحزن والعجز والكسل والفقر والجوع والجبن والبخل وغلبة الدين وقهر الرجال",
+                // Add your own custom prayer messages here
+            };
+
+            string prayer = prayers[Counter]; 
+            Counter++; 
+
+            
+            if (Counter >= prayers.Length)
+            {
+                Counter = 0;
+            }
+
+            return prayer;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            notifyIcon1.Icon = SystemIcons.Information;
+            notifyIcon1.BalloonTipText = TextGenerator();
+            notifyIcon1.ShowBalloonTip(10);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+            timer1.Interval = (Convert.ToInt32(txtNumberOfPrayers.Text) * 1000);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            progress++;
+            label1.Text = progress.ToString();
+            
+
+
+            if (progress==timer1.Interval/1000)
+            {
+                
+                progress = 0;
+            }
+            
+        }
+        void Clear()
+        {
+            timer1.Stop();
+            timer2.Stop();
+            label1.Text = "0";
+            progress = 0;
+        }
+        private void txtNumberOfPrayers_TextChanged(object sender, EventArgs e)
+        {
+            Clear();
+            if (txtNumberOfPrayers.Text == "")
+            {
+                txtNumberOfPrayers.Text = "10";
+            }
+            timer1.Interval = (Convert.ToInt32(txtNumberOfPrayers.Text) * 1000);
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+    
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
